003a49193fdef0ea04e8ddf0b30c39ba11a41d7f
Harsh Jethava <105279997+harshjethava@users.noreply.github.com>
2026-07-15 17:04:39 +0530

    Time: 0 ms (100%), Space: 42.5 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,27 +1,28 @@
 class Solution {
 
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = a % b;
-            a = b;
-            b = t;
-        }
-        return a;
-    }
+    // private int gcd(int a, int b) {
+    //     while (b != 0) {
+    //         int t = a % b;
+    //         a = b;
+    //         b = t;
+    //     }
+    //     return a;
+    // }
     public int gcdOfOddEvenSums(int n) {
 
-        int sumOdd = 0, sumEven = 0;
-        int i=0;
-        int startOdd = 1, startEven = 2;
+        // int sumOdd = 0, sumEven = 0;
+        // int i=0;
+        // int startOdd = 1, startEven = 2;
 
-        while(i < n){
-            sumOdd = sumOdd + startOdd;
-            sumEven = sumEven + startEven;
-            startOdd = startOdd + 2;
-            startEven = startEven + 2;
-            i++;
-        }
+        // while(i < n){
+        //     sumOdd = sumOdd + startOdd;
+        //     sumEven = sumEven + startEven;
+        //     startOdd = startOdd + 2;
+        //     startEven = startEven + 2;
+        //     i++;
+        // }
 
-        return gcd(sumOdd, sumEven);
+        // return gcd(sumOdd, sumEven);
+        return n;
     }
 }
