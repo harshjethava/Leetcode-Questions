57b30c31f8daa09b853fa966ca0d4dbd6c8e3ecc
Harsh Jethava <105279997+harshjethava@users.noreply.github.com>
2026-08-02 18:09:21 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.69%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+
+        int len = piles.length;
+        int alice = 0, bob = 0;
+
+        for(int i=0;i<len;i++){
+            if(i%2 == 0){
+                alice += piles[i];
+            }else{
+                bob += piles[i];
+            }
+        }
+        
+        if(alice > bob) return true;
+
+        alice = 0; bob = 0;
+
+        if(len % 2 == 0){
+            for(int j=len-1;j>=0;j--){
+                if(j%2 != 0){
+                    alice += piles[j];  
+                }else{
+                    bob += piles[j];
+                }
+            }
+        }else{
+            for(int j=len-1;j>=0;j--){
+                if(j%2 == 0){
+                    alice += piles[j];
+                }else{
+                    bob += piles[j];
+                }
+            }
+        }
+
+        if(alice > bob) return true;
+        return false;
+    }
+}
