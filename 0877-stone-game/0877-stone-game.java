d4ae7e157afb12ed2f7eb5f1253a1ea11dfeef25
Harsh Jethava <105279997+harshjethava@users.noreply.github.com>
2026-08-03 15:28:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.6%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,42 +1,43 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
 
-        int len = piles.length;
-        int alice = 0, bob = 0;
+        // int len = piles.length;
+        // int alice = 0, bob = 0;
         
-        //From Start
-        for(int i=0;i<len;i++){
-            if(i%2 == 0){
-                alice += piles[i];
-            }else{
-                bob += piles[i];
-            }
-        }
+        // //From Start
+        // for(int i=0;i<len;i++){
+        //     if(i%2 == 0){
+        //         alice += piles[i];
+        //     }else{
+        //         bob += piles[i];
+        //     }
+        // }
         
-        if(alice > bob) return true;
+        // if(alice > bob) return true;
 
-        alice = 0; bob = 0;
+        // alice = 0; bob = 0;
 
-        //From End
-        if(len % 2 == 0){
-            for(int j=len-1;j>=0;j--){
-                if(j%2 != 0){
-                    alice += piles[j];  
-                }else{
-                    bob += piles[j];
-                }
-            }
-        }else{
-            for(int j=len-1;j>=0;j--){
-                if(j%2 == 0){
-                    alice += piles[j];
-                }else{
-                    bob += piles[j];
-                }
-            }
-        }
+        // //From End
+        // if(len % 2 == 0){
+        //     for(int j=len-1;j>=0;j--){
+        //         if(j%2 != 0){
+        //             alice += piles[j];  
+        //         }else{
+        //             bob += piles[j];
+        //         }
+        //     }
+        // }else{
+        //     for(int j=len-1;j>=0;j--){
+        //         if(j%2 == 0){
+        //             alice += piles[j];
+        //         }else{
+        //             bob += piles[j];
+        //         }
+        //     }
+        // }
 
-        if(alice > bob) return true;
-        return false;
+        // if(alice > bob) return true;
+        // return false;
+        return true;
     }
 }
