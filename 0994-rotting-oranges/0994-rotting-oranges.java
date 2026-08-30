3c53f2bbe9723a250b25c04ad81f6a129c2f9b0a
Harsh Jethava <105279997+harshjethava@users.noreply.github.com>
2026-08-30 12:18:28 +0530

    Time: 2 ms (89.44%), Space: 44.2 MB (40.06%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans = 0;
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 2){
+                    queue.add(new int[]{i, j, 0});
+                }
+            }
+        }
+
+        while(queue.size() > 0){
+
+            int curr[] = queue.poll();
+            int i = curr[0];
+            int j = curr[1];
+            int time = curr[2];
+
+            ans = Math.max(ans,time);
+
+            if(i-1 >= 0 && !visited[i-1][j] && grid[i-1][j] == 1){ // top
+                queue.add(new int[]{i-1, j, time + 1});
+                visited[i-1][j] = true;
+            }
+            
+            if(i+1 < m && !visited[i+1][j] && grid[i+1][j] == 1){ // bottom
+                queue.add(new int[]{i+1, j, time + 1});
+                visited[i+1][j] = true;
+            }
+
+            if(j-1 >= 0 && !visited[i][j-1] && grid[i][j-1] == 1){ // left
+                queue.add(new int[]{i, j-1, time + 1});
+                visited[i][j-1] = true;
+            }
+
+            if(j+1 < n && !visited[i][j+1] && grid[i][j+1] == 1){ // right
+                queue.add(new int[]{i, j+1, time + 1});
+                visited[i][j+1] = true;
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    return -1;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
